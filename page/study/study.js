0ba854ef10f0af7c39e85d615be35129a5b21105
floadyun <floadyun@163.com>
2018-03-12 17:18:33 +0800

    微信小程序学习

---
+++ page/study/study.js
@@ -0,0 +1,70 @@
+// page/study/study.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    avator_url:"http://t-carrierdata.oss-cn-shenzhen.aliyuncs.com/img/users/uploads/18/02/09/5a7cfcbcbd253.jpeg",
+    username:"floadyun"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  buttonClick:function(e){
+    console.log('点击事件')
+  }
+})
